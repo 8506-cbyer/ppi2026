5421a8b684ca9ff50c54e9260a002eb858b71ef5
8506-cbyer <victor.juvencio@escolar.ifrn.edu.br>
2026-05-06 19:19:19 +0000

    Aula 06/05 - Introdução a Javascript

---
+++ ppi2026.js
@@ -0,0 +1,30 @@
+console.log("Bem vindo a PPI");
+// Variáveis
+let name = "Victor";
+let age = 17;
+let isStudent = true;
+
+const PI = 3.14; // Não pode mudar de valor
+console.log(PI);
+
+// Operadores Aritméticos + - / * **
+let x = 5 + 5; // soma
+let y = "5" + 5; // concatenação
+let z = "Hello" + 5;
+
+console.log(x**2, y, z); // potência
+console.log(typeof x);
+
+// Ativar o formatador Prettier
+// ALT + SHIFT + F
+
+// Operadores relacionais 
+console.log('5' != 5); // Diferença
+console.log('5' == 5); // Compara tipo OU valor 
+console.log('5' === 5); // Compara tipo E valor
+
+// Operadores de incremento
+console.log(x++); // incremento após 
+console.log(x);
+console.log(++x);
+
